be0081a4a688d36b17737afb445c7efac5389023
Armin Becher <armin.becher@gmail.com>
2019-07-21 00:29:13 +0200

    Simplified method call parameter

--- src/file_traversal.rs
+++ src/file_traversal.rs
@@ -2,8 +2,8 @@ extern crate walkdir;
 
 use walkdir::WalkDir;
 
-pub fn find<T: AsRef<str>>(root: &str, file_extensions: &[T], result: &mut Vec<String>) {
-    info!("Search for files with extension '{}' in directory '{}'", to_string(file_extensions), root);
+pub fn find(root: &str, file_extensions: &[String], result: &mut Vec<String>) {
+    info!("Search for files with extension '{:?}' in directory '{}'", file_extensions, root);
     for entry in WalkDir::new(root)
         .follow_links(false)
         .into_iter()
@@ -17,30 +17,14 @@ pub fn find<T: AsRef<str>>(root: &str, file_extensions: &[T], result: &mut Vec<S
     }
 }
 
-fn has_file_extension<T: AsRef<str>>(file: &str, file_extensions: &[T]) -> bool {
+fn has_file_extension(file: &str, file_extensions: &[String]) -> bool {
     let file_low = file.to_lowercase();
     for ext in file_extensions {
-        let ext_low = ext.as_ref().to_lowercase();
+        let ext_low = ext.to_lowercase();
         if file_low.ends_with(&ext_low) {
             return true;
         }
     }
 
     false
-}
-
-fn to_string<T: AsRef<str>>(file_extensions: &[T]) -> String {
-    let mut result = String::new();
-    result.push_str("[");
-    for (i, ext) in file_extensions.iter().enumerate() {
-        result.push_str("\"");
-        result.push_str(ext.as_ref());
-        if (i + 1) < file_extensions.len() {
-            result.push_str("\", ");
-        } else {
-            result.push_str("\"");
-        }
-    }
-    result.push_str("]");
-    result
 }
--- tests/file_traversal.rs
+++ tests/file_traversal.rs
@@ -1,10 +1,9 @@
 #[cfg(test)]
-
 use linkchecker::file_traversal;
 use std::env;
-use std::path::PathBuf;
+use std::path::{PathBuf, Path};
 
-fn root_dir() -> String{
+fn root_dir() -> String {
     let default_path = PathBuf::from(r"/");
     let path = env::current_dir().unwrap_or(default_path).to_string_lossy().to_string();
     path
@@ -13,20 +12,21 @@ fn root_dir() -> String{
 #[test]
 fn find_markdown_files() {
     let mut path = root_dir();
-    let file_extension = [".md"];
+    let file_extension = [".md".to_string()];
     path.push_str("/tests/traversal_test/three_empty_md_files");
     let mut result: Vec<String> = Vec::new();
     file_traversal::find(&path, &file_extension, &mut result);
     assert_eq!(result.len(), 3);
-    assert!(result.contains(&"f1.md".to_string()));
-    assert!(result.contains(&"f2.MD".to_string()));
-    assert!(result.contains(&"F3_with_umlaut.md".to_string()));
+    let possible_results = ["f1.md", "f2.MD", "F3_with_umlaut.md"];
+    for r in result {
+        assert!(possible_results.contains(&Path::new(&r).file_name().unwrap().to_str().unwrap()));
+    }
 }
 
 #[test]
 fn empty_folder() {
     let mut path = root_dir();
-    let file_extension = [".md"];
+    let file_extension = [".md".to_string()];
     path.push_str("/tests/traversal_test/empty");
     let mut result: Vec<String> = Vec::new();
     file_traversal::find(&path, &file_extension, &mut result);
