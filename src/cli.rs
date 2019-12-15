fa2bb87af170ca04ef1500b75514e173dd65e670
Armin Becher <armin.becher@gmail.com>
2019-12-15 18:39:00 +0100

    Version info from crate

--- src/cli.rs
+++ src/cli.rs
@@ -1,18 +1,25 @@
-use crate::Config;
 use crate::logger;
-use clap::App;
 use crate::markup::MarkupType;
-
+use crate::Config;
+use clap::App;
 
 pub fn parse_args() -> Config {
     let yaml = load_yaml!("cli.yml");
-    let matches = App::from_yaml(yaml).get_matches();
+    let matches = App::from_yaml(yaml).version(crate_version!()).get_matches();
     let debug = matches.is_present("debug");
-    let log_level = if debug { logger::LogLevel::Debug } else { logger::LogLevel::Warn };
+    let log_level = if debug {
+        logger::LogLevel::Debug
+    } else {
+        logger::LogLevel::Warn
+    };
     let folder = matches.value_of("folder").unwrap_or("./");
     let markup_types = vec![MarkupType::Markdown]; //TODO read from cli
     debug!("The log level is: {}", log_level);
     debug!("The root folder is: {:?}", folder);
     debug!("The file extension are: {:?}", markup_types);
-    Config { log_level, folder: folder.parse().unwrap(), markup_types }
+    Config {
+        log_level,
+        folder: folder.parse().unwrap(),
+        markup_types,
+    }
 }
