57f85e30138daf8ea458cb40819ca40bf2d96f9f
Armin Becher <armin.becher@gmail.com>
2019-12-17 23:43:18 +0100

    Init benches

--- tests/end_to_end.rs
+++ tests/end_to_end.rs
@@ -12,3 +12,10 @@ fn end_to_end() {
     };
     let _ = mlc::run(&config);
 }
+
+#[bench]
+fn bench_xor_1000_ints(b: &mut Bencher) {
+    b.iter(|| {
+        (0..1000).fold(0, |old, new| old ^ new);
+    });
+}
