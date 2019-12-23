e33eec1bb4e8c728907d7cdd41f3f83c47828f59
Armin Becher <armin.becher@gmail.com>
2019-12-23 10:01:32 +0100

    Support more markdown file endings

--- src/markup.rs
+++ src/markup.rs
@@ -1,5 +1,5 @@
 #[derive(Debug)]
-pub struct MarkupFile{
+pub struct MarkupFile {
     pub markup_type: MarkupType,
     pub path: String,
 }
@@ -13,25 +13,19 @@ pub enum MarkupType {
 impl MarkupType {
     pub fn file_extensions(&self) -> Vec<String> {
         match self {
-            MarkupType::Markdown => vec!["md".to_string()],
+            MarkupType::Markdown => vec![
+                "md".to_string(),
+                "markdown".to_string(),
+                "mkdown".to_string(),
+                "mkdn".to_string(),
+                "mkd".to_string(),
+                "mdwn".to_string(),
+                "mdtxt".to_string(),
+                "mdtext".to_string(),
+                "text".to_string(),
+                "rmd".to_string(),
+            ],
             MarkupType::HTML => vec!["html".to_string(), "xhtml".to_string()],
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn markdown_type_file_endings() {
-        let md = MarkupType::Markdown;
-        assert_eq!(vec!["md".to_string()], md.file_extensions())
-    }
-
-    #[test]
-    fn html_type_file_endings() {
-        let md = MarkupType::HTML;
-        assert_eq!(vec!["html".to_string(), "xhtml".to_string()], md.file_extensions())
     }
 }
--- tests/file_traversal.rs
+++ tests/file_traversal.rs
@@ -2,11 +2,10 @@
 use mlc::file_traversal;
 use mlc::markup::{MarkupFile, MarkupType};
 use mlc::Config;
-use std::path::Path;
 
 #[test]
 fn find_markdown_files() {
-    let path = "./benches/benchmark/three_empty_md_files".to_string();
+    let path = "./benches/benchmark/md_file_endings".to_string();
     let config: Config = Config {
         folder: path,
         markup_types: vec![MarkupType::Markdown],
@@ -15,13 +14,7 @@ fn find_markdown_files() {
     let mut result: Vec<MarkupFile> = Vec::new();
 
     file_traversal::find(&config, &mut result);
-    assert_eq!(result.len(), 3);
-    let possible_results = ["f1.md", "f2.MD", "F3_with_umlaut.md"];
-    for r in result {
-        let path = r.path;
-        let file_names = &Path::new(&path).file_name().unwrap().to_str().unwrap();
-        assert!(possible_results.contains(file_names));
-    }
+    assert_eq!(result.len(), 12);
 }
 
 #[test]
