534bf634f2c30e912b4cfc574a9739340f9f5532
Armin Becher <armin.becher@methodpark.de>
2020-07-12 20:57:40 +0200

    Update async fs calls

--- src/link_validator/file_system.rs
+++ src/link_validator/file_system.rs
@@ -1,10 +1,10 @@
 use crate::Config;
 use crate::link_validator::LinkCheckResult;
-use std::path::Path;
+use async_std::path::Path;
 use std::path::MAIN_SEPARATOR;
-use std::path::PathBuf;
+use async_std::path::PathBuf;
 
-pub fn check_filesystem(source: &str, target: &str, config: &Config) -> LinkCheckResult {
+pub async fn check_filesystem(source: &str, target: &str, config: &Config) -> LinkCheckResult {
     let mut normalized_link = target
         .replace('/', &MAIN_SEPARATOR.to_string())
         .replace('\\', &MAIN_SEPARATOR.to_string());
@@ -17,7 +17,7 @@ pub fn check_filesystem(source: &str, target: &str, config: &Config) -> LinkChec
     }
     let mut fs_link_target = Path::new(&normalized_link).to_path_buf();
     if normalized_link.starts_with(MAIN_SEPARATOR) && config.root_dir.is_some() {
-        match std::fs::canonicalize(&config.root_dir.as_ref().unwrap()) {
+        match async_std::fs::canonicalize(&config.root_dir.as_ref().unwrap()).await {
             Ok(new_root) => fs_link_target = new_root.join(Path::new(&normalized_link[1..])),
             Err(e) => panic!(
                 "Root path could not be converted to an absolute path. Does the directory exit? {}",
@@ -29,7 +29,7 @@ pub fn check_filesystem(source: &str, target: &str, config: &Config) -> LinkChec
     debug!("Check file system link target {:?}", target);
     let target = absolute_target_path(source, &fs_link_target);
     debug!("Absolute target path {:?}", target);
-    if target.exists() {
+    if target.exists().await {
         LinkCheckResult::Ok
     } else {
         LinkCheckResult::Failed("Target path not found.".to_string())
