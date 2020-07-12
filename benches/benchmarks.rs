44179d20d7541af01e740e702144f5dc498c9172
Armin Becher <armin.becher@methodpark.de>
2020-07-12 15:31:36 +0200

    Fix bench

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -2,16 +2,19 @@
 extern crate criterion;
 
 use criterion::Criterion;
+use std::path::Path;
 use mlc::logger;
 use mlc::markup::MarkupType;
 use mlc::Config;
 
 fn end_to_end_benchmark() {
     let config = Config {
-        folder: String::from("./benches/benchmark"),
+        folder: Path::new("./benches/benchmark").to_path_buf(),
         log_level: logger::LogLevel::Debug,
         markup_types: vec![MarkupType::Markdown],
         ignore_links: vec![],
+        ignore_path: vec![],
+        root_dir: None,
         no_web_links: false,
     };
     let _ = mlc::run(&config);
