9fb6b66bc57015eeb97e430984b04bff89b7fcfe
hoijui <hoijui.quaero@gmail.com>
2021-11-16 13:54:55 +0100

    `cargo fmt` tests and benchmarks

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,13 +1,12 @@
 #[cfg(test)]
-
 #[macro_use]
 extern crate criterion;
 
 use criterion::Criterion;
-use std::fs;
 use mlc::logger;
 use mlc::markup::MarkupType;
 use mlc::Config;
+use std::fs;
 
 fn end_to_end_benchmark() {
     let config = Config {
@@ -25,7 +24,9 @@ fn end_to_end_benchmark() {
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("End to end benchmark", |b| b.iter(|| end_to_end_benchmark()));
+    c.bench_function("End to end benchmark", |b| {
+        b.iter(|| end_to_end_benchmark())
+    });
 }
 
 criterion_group! {
